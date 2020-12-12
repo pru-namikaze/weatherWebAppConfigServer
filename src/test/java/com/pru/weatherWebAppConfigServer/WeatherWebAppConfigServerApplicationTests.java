a36bcdc48e10bafaee7ddb3a8dae3805c3d92ade
Pranshu <pragoel2@lowes.com>
2020-12-12 18:35:30 +0530

    Initial Commit

---
+++ src/test/java/com/pru/weatherWebAppConfigServer/WeatherWebAppConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pru.weatherWebAppConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherWebAppConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
